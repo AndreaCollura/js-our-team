68b6665197849f939fd916895eadab12932cf602
AndreaCollura <andrea.collur@gmail.com>
2023-03-22 15:30:53 +0100

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,92 @@
+/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede */
+
+
+/* NAME     SURNAME    ROLE                      IMAGE
+Wayne    Barnett	Founder & CEO	          wayne-barnett-founder-ceo.jpg
+Angela   Caroll	    Chief Editor	          angela-caroll-chief-editor.jpg
+Walter   Gordon	    Office Manager	          walter-gordon-office-manager.jpg
+Angela   Lopez	    Social Media Manager	  angela-lopez-social-media-manager.jpg
+Scott    Estrada	Developer	              scott-estrada-developer.jpg
+Barbara  Ramos	    Graphic Designer	      barbara-ramos-graphic-designer.jpg */
+
+
+
+
+// creo array di oggetti inserendo i dati fortiti
+
+
+
+
+// stampo con log tutti i dati in nostro possesso piu stinga foto
+
+
+
+
+
+// devo riportare in pagina con innerHTML/Text i dati sooto forma di stringa x ogni chiave (nome, cognome, role, ecc)
+
+
+
+
+
+
+
+////////////////////////////////////////////////////
+
+
+
+
+//  fatto tutto sopra devo inserire in pagina tutti i dati dell'array dentro delle card
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
